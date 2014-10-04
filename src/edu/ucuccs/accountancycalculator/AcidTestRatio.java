36b6cc5a944e01ae401827c00b6487e4b14ab97d
emilyn1402 <emilyn1402@gmail.com>
2014-10-04 15:58:03 -0700

    Acid Test Ratio was created
    Acid Test Ratio for Category Liquidity was Created

---
+++ src/edu/ucuccs/accountancycalculator/AcidTestRatio.java
@@ -0,0 +1,95 @@
+package edu.ucuccs.accountancycalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class AcidTestRatio extends Activity {
+	
+	EditText editcass;
+	EditText editinvent;
+	EditText editclia;
+	Button btncal;
+	TextView txtans;
+	
+	Double ass, invent, lia, answer;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_acid_test_ratio);
+		
+		editcass = (EditText) findViewById(R.id.editcass);
+		editinvent = (EditText) findViewById(R.id.editinvent);
+		editclia = (EditText) findViewById(R.id.editclia);
+		btncal = (Button) findViewById(R.id.btncal);
+		txtans = (TextView) findViewById(R.id.txtans);
+		
+		btncal.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+				if (editcass.getText().toString().equals("")) {
+
+					Toast.makeText(getApplicationContext(),
+							"Please Fill up all!", Toast.LENGTH_LONG).show();
+
+					if (editinvent.getText().toString().equals(0)) {
+
+						Toast.makeText(getApplicationContext(),
+								"Please Fill up all!", Toast.LENGTH_LONG)
+								.show();
+						
+						if (editclia.getText().toString().equals(0)) {
+
+							Toast.makeText(getApplicationContext(),
+									"Please Fill up all!", Toast.LENGTH_LONG)
+									.show();
+
+						}
+
+					}
+
+				} else if (editinvent.getText().toString().equals("")) {
+
+					Toast.makeText(getApplicationContext(),
+							"Please Fill up all!", Toast.LENGTH_LONG).show();
+					
+					if (editclia.getText().toString().equals(0)) {
+
+						Toast.makeText(getApplicationContext(),
+								"Please Fill up all!", Toast.LENGTH_LONG)
+								.show();
+
+					}
+				
+
+				} else if (editclia.getText().toString().equals(0)) {
+
+					Toast.makeText(getApplicationContext(),
+							"Please Fill up all!", Toast.LENGTH_LONG)
+							.show();
+
+				}  else {
+
+					ass = Double.parseDouble(editcass.getText().toString());
+					invent = Double.parseDouble(editinvent.getText().toString());
+					lia = Double.parseDouble(editclia.getText().toString());
+					
+					answer = ass - invent;
+					answer = answer / lia;
+					
+					txtans.setText(answer + "");
+
+				}
+				
+			}});
+	}
+}
--- src/edu/ucuccs/accountancycalculator/Liquidity.java
+++ src/edu/ucuccs/accountancycalculator/Liquidity.java
@@ -1,22 +1,72 @@
 package edu.ucuccs.accountancycalculator;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Liquidity extends Activity {
+	
+	Button btnacid;
+	Button btncash;
+	Button btncurrent;
+	Button btnoperation;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_liquidity);
-	}
+		
+		btnacid = (Button) findViewById(R.id.btnacid);
+		btncash = (Button) findViewById(R.id.btncash);
+		btncurrent = (Button) findViewById(R.id.btncurrent);
+		btnoperation = (Button) findViewById(R.id.btnoperation);
+		
+		btnacid.setOnClickListener(new OnClickListener(){
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.liquidity, menu);
-		return true;
-	}
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+				Intent gotodebtequity = new Intent (Liquidity.this, AcidTestRatio.class);
+				startActivity(gotodebtequity);
+				
+			}});
+		
+		
+		btncash.setOnClickListener(new OnClickListener(){
 
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+				Intent gotodebtratio = new Intent (Liquidity.this, DebtRatios.class);
+				startActivity(gotodebtratio);
+				
+			}});
+		
+		btncurrent.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+				Intent gotodebtservice = new Intent (Liquidity.this, DebtServiceCourage.class);
+				startActivity(gotodebtservice);
+				
+			}});
+		
+		btnoperation.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+				Intent gotolongterm = new Intent (Liquidity.this, LongTermDebtEquity.class);
+				startActivity(gotolongterm);
+				
+			}});
+	}
 }
