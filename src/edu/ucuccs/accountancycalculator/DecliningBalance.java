08280611ba5388d722d478025609bc8427e7c230
emilyn1402 <emilyn1402@gmail.com>
2014-10-10 21:29:42 -0700

    Declining Balance Created
    for Category Depreciation

---
+++ src/edu/ucuccs/accountancycalculator/DecliningBalance.java
@@ -0,0 +1,70 @@
+package edu.ucuccs.accountancycalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DecliningBalance extends Activity {
+	
+	EditText editdepre;
+	EditText editbook;
+	Button btncal;
+	TextView txtans;
+
+	double depre, book, answer;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_declining_balance);
+		editdepre = (EditText) findViewById(R.id.editdepre);
+		editbook = (EditText) findViewById(R.id.editbook);
+		btncal = (Button) findViewById(R.id.btncal);
+		txtans = (TextView) findViewById(R.id.txtans);
+		
+		btncal.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+				if (editdepre.getText().toString().equals("")) {
+
+					Toast.makeText(getApplicationContext(),
+							"Please Fill up all!", Toast.LENGTH_LONG).show();
+
+					if (editbook.getText().toString().equals(0)) {
+
+						Toast.makeText(getApplicationContext(),
+								"Please Fill up all!", Toast.LENGTH_LONG)
+								.show();
+
+					}
+
+				} else if (editbook.getText().toString().equals("")) {
+
+					Toast.makeText(getApplicationContext(),
+							"Please Fill up all!", Toast.LENGTH_LONG).show();
+
+				} else {
+
+					depre = Double.parseDouble(editdepre.getText()
+							.toString());
+					book = Double
+							.parseDouble(editbook.getText().toString());
+
+					answer = depre * book;
+
+					txtans.setText(answer + "");
+
+				}
+
+				
+			}});
+	}
+}
